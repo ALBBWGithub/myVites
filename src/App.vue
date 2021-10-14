2bad3ac45bc3ecaa5c74e37645a098efd8e29233
Denis Heimburger <5628938-Ausbilder@users.noreply.gitlab.com>
2021-10-14 18:15:50 +0200

    kickma

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,20 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+let meineFunktion = function name(msg:String) {
+
+  return msg;
+}
+
+let var1 = meineFunktion('Das ist ein Test');
+
+
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld class="button" msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
@@ -15,7 +24,7 @@ import HelloWorld from './components/HelloWorld.vue'
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #83b0dd;
   margin-top: 60px;
 }
 </style>
